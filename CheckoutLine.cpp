2ff81b5e453462ac49a79b305df978a6856ec0ee
Geshlee Ruiz <139276311+gruizg@users.noreply.github.com>
2025-02-28 02:47:46 -0500

    Update CheckoutLine.cpp

--- CheckoutLine.cpp
+++ CheckoutLine.cpp
@@ -15,6 +15,7 @@ static std::mt19937 gen(ran());
 
 std::mutex mutexA;
 std::mutex mutexB;
+std::mutex printMutex;
 
 int CheckoutLine::nextId = 1;
 
@@ -45,15 +46,33 @@ void CheckoutLine::processCustomers(Store& s) {
         Customer c = dequeueCustomer();
 
         if (c.getIsReturn()) {
-            s.refund(c);
+            while (!mutexA.try_lock()) {}
+            while (!mutexB.try_lock()) {
+                mutexA.unlock();
+                std::this_thread::yield();
+            }
+        } else {
+            while (!mutexB.try_lock()) {}
+            while (!mutexA.try_lock()) {
+                mutexB.unlock();
+                std::this_thread::yield();
+            }
         }
-        else {
+
+        if (c.getIsReturn()) {
+            s.refund(c);
+        } else {
             s.purchase(c);
         }
-        
-        std::cout << "Processing line " << getId() << " " << c << "\n";
-        std::cout << "Store Balance: " << s.getBalance() << "\n";
 
+        mutexA.unlock();
+        mutexB.unlock();
+
+        {
+            std::lock_guard<std::mutex> printLock(printMutex);
+            std::cout << "Processing line " << getId() << " " << c << "\n";
+            std::cout << "Store Balance: " << s.getBalance() << "\n";
+        }
     }
 }
 

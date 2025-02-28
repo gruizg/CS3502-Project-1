a36a6bfd41b9ebc2d3f8a230c9cb310f299c840d
Geshlee Ruiz <139276311+gruizg@users.noreply.github.com>
2025-02-28 02:20:13 -0500

    Update Customer.h

--- Customer.h
+++ Customer.h
@@ -2,8 +2,13 @@
 // Created by Geshlee Ruiz on 2/20/25.
 //
 
+/*
+
+*/
+
 #ifndef CUSTOMER_H
 #define CUSTOMER_H
+
 #include <ostream>
 
 class Customer {
@@ -11,6 +16,7 @@ private:
     int itemNumber;
     double itemPrice;
     bool isReturn;
+
     static constexpr double storePrices[] = {1.99, 4.99, 9.99, 14.99, 19.99, 24.99, 49.99, 99.99, 249.99};
 
 public:
@@ -22,5 +28,6 @@ public:
 
     friend std::ostream& operator << (std::ostream& os, const Customer& c);
 };
+
 
 #endif //CUSTOMER_H

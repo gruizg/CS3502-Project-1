7301cd50e24f0d529f8564eb21f27fa4aec053d1
Geshlee Ruiz <139276311+gruizg@users.noreply.github.com>
2025-02-28 16:41:33 +0000

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,15 +9,18 @@ int main() {
 
     //Implements user input to allow test cases with different numbers of checkout lines
     int lines;
+    int phase;
     std::cout << "Department Store Simulator\n";
 
     std::cout << "How many lines will your store have? ";
     std::cin >> lines;
+    std::cout << "Which phase to run? (1-4)";
+    std::cin >>phase;
     std::cout << "Running store...\n";
 
     //Create store and call method to run store simulation
     Store myStore(lines);
-    myStore.runStore();
+    myStore.runStore(phase);
 
     return 0;
 }
